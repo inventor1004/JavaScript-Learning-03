051944300839550831bc4ef45eca03648128c7b7
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-09-24 16:21:29 -0400

    How to use addEventListener. It can add new event on the webpage based on the second function argument which is passed as a second parameter in the addEventListener.

--- script.js
+++ script.js
@@ -4,13 +4,24 @@
 /* DOM: Document Object Model */
 /* The DOM allows us to use JavaScript to access HTML elememts and styles in order to manipulate them*/
 
+/***** DOM Manipulation *************************************/
 console.log(document.querySelector('.message').textContent);
 
 // Manipulate the text messages
 document.querySelector('.message').textContent = 'New Player!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 20;
-
-// Getting user input
 document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+
+
+/***** Events **********************************************/
+// Getting user input
+function getInput ()
+{
+  console.log(document.querySelector('.guess').value);
+}
+
+// method '.addEventListener' need function as a second parameter.
+// It adds certain event on the webpage based on the condition which the second function argument has
+// In below case, it log the user entered value when the button '.check' is clicked 
+document.querySelector('.check').addEventListener('click', getInput);
