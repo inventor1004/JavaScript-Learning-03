0941857142801a5d9b56428123c49367e2aceccb
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-09-24 16:10:24 -0400

    Practice how to manipulate the texts in a web using DOM.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+'use strict';
+
+/* DOM: Document Object Model */
+/* The DOM allows us to use JavaScript to access HTML elememts and styles in order to manipulate them*/
+
+console.log(document.querySelector('.message').textContent);
+
+// Manipulate the text messages
+document.querySelector('.message').textContent = 'New Player!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// Getting user input
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
