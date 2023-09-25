32c9389396b701b6c142208db4a77925e9d97d16
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-09-25 09:20:00 -0400

    Add some more detailed condition about scoring system.

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ console.log(document.querySelector('.message').textContent);
 // Manipulate the text messages
 document.querySelector('.message').textContent = 'New Player!';
 document.querySelector('.number').textContent = '?';
-document.querySelector('.score').textContent = 0;
+document.querySelector('.score').value = 20;
+document.querySelector('.score').textContent = 20;
 document.querySelector('.guess').value = 0;
 
 
@@ -19,28 +20,39 @@ document.querySelector('.guess').value = 0;
 // create function for which we want to perform certain envent on the web page
 function getInput (ranNum)
 {
-  // read uesr's number selection 
-  console.log(document.querySelector('.guess').value);
-
   // check user input and its data type
   const guess = Number(document.querySelector('.guess').
   value); 
   console.log(guess, typeof guess)  
-  
-// display accurate text based on the game rule
+
+  // display accurate text based on the game rule
   if (guess === ranNum) {
     document.querySelector('.message').textContent = 'Correct Number. You Win!';
     document.querySelector('.number').textContent = ranNum;
+
+    document.querySelector('.highscore').textContent = score;
   }
   else if (guess < ranNum) {
-    document.querySelector('.message').textContent = 'It is higher than that number!';
-    
+    if (score > 0){
+      document.querySelector('.message').textContent = 'It is higher than that number!';
+
+    --score;
+    document.querySelector('.score').textContent = score;
+    }
   }
   else if (guess > ranNum) {
-    document.querySelector('.message').textContent = 'It is lower than that number!';
-    
+    if (score > 0){
+      document.querySelector('.message').textContent = 'It is lower than that number!';
+
+      --score;
+      document.querySelector('.score').textContent = score;
+    }
   }
   
+  if (score === 0)
+  {
+    document.querySelector('.message').textContent = "You lost the game. Let's try again!";
+  }
 
 }
 
@@ -49,6 +61,9 @@ function getInput (ranNum)
 // In below case, it log the user entered value when the button '.check' is clicked 
 let ranNum = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = ranNum;
+let score = Number(document.querySelector('.score').
+value); 
+
 document.querySelector('.check').addEventListener('click', function() {getInput(ranNum)});
 
 
