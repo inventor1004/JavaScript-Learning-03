c672886cd17019662e319bd0de96e5b3d03a9ba7
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-09-25 08:46:30 -0400

    Add the condition of the game and its insturction.

--- script.js
+++ script.js
@@ -9,19 +9,50 @@ console.log(document.querySelector('.message').textContent);
 
 // Manipulate the text messages
 document.querySelector('.message').textContent = 'New Player!';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-document.querySelector('.guess').value = 23;
+document.querySelector('.number').textContent = '?';
+document.querySelector('.score').textContent = 0;
+document.querySelector('.guess').value = 0;
 
 
 /***** Events **********************************************/
-// Getting user input
-function getInput ()
+/**  Getting user input **/
+// create function for which we want to perform certain envent on the web page
+function getInput (ranNum)
 {
+  // read uesr's number selection 
   console.log(document.querySelector('.guess').value);
+
+  // check user input and its data type
+  const guess = Number(document.querySelector('.guess').
+  value); 
+  console.log(guess, typeof guess)  
+  
+// display accurate text based on the game rule
+  if (guess === ranNum) {
+    document.querySelector('.message').textContent = 'Correct Number. You Win!';
+    document.querySelector('.number').textContent = ranNum;
+  }
+  else if (guess < ranNum) {
+    document.querySelector('.message').textContent = 'It is higher than that number!';
+    
+  }
+  else if (guess > ranNum) {
+    document.querySelector('.message').textContent = 'It is lower than that number!';
+    
+  }
+  
+
 }
 
 // method '.addEventListener' need function as a second parameter.
 // It adds certain event on the webpage based on the condition which the second function argument has
 // In below case, it log the user entered value when the button '.check' is clicked 
-document.querySelector('.check').addEventListener('click', getInput);
+let ranNum = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = ranNum;
+document.querySelector('.check').addEventListener('click', function() {getInput(ranNum)});
+
+
+/*
+ 
+
+*/
